62c55fef6731556acb205934168f05f927a83083
Sylvain CONSO <sconso@student.42.fr>
2015-01-15 23:56:08 +0100

    ex01 finished

--- J08/ex01/main.cpp
+++ J08/ex01/main.cpp
@@ -6,7 +6,7 @@
 //   By: Myrkskog <marvin@42.fr>                    +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2015/01/15 19:27:25 by Myrkskog          #+#    #+#             //
-//   Updated: 2015/01/15 19:50:50 by Myrkskog         ###   ########.fr       //
+//   Updated: 2015/01/15 23:55:19 by Myrkskog         ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -15,21 +15,56 @@
 
 int main(void)
 {
-	Span tab(10);
-	srand(time(NULL));
+	{
+		Span sp = Span(5);
+
+		try {
+			sp.shortestSpan();
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		try {
+			sp.addNumber(5);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			sp.addNumber(42);
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "Min: " << sp.shortestSpan() << std::endl;
+		std::cout << "Max: " << sp.longestSpan() << std::endl;
+	}
+
+	Span tab(10000);
+// 	try {
+// 		for (int i = 0; i < 5; ++i)
+// 			tab.addNumber(rand() % 1111);
+// 	}
+// 	catch (std::exception &e) {
+// 		std::cout << e.what() << std::endl;
+// 	}
+
+//	tab.dump();
+//	tab.addRange(tab.begin(), tab.end());
+//	tab.addNumber(tab.begin(), 42);
+//	tab.dump();
 
 	try {
-		for (int i = 0; i < 11; ++i)
-			tab.addNumber(rand() % 1111);
+		tab.addRange(tab.begin(), tab.end() + 500);
 	}
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
-	}
 
-	tab.dump();
+	}
 	std::cout << "Min span = " << tab.shortestSpan() << std::endl;
 	std::cout << "Max span = " << tab.longestSpan() << std::endl;
-	tab.dump();
+//	tab.dump();
 
 
 	return 0;
